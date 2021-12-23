39ed93424ca80e5a8507e9ca1d7bf09b8e37b6e8
JeffXu <xuzj@shanghaitech.edu.cn>
2021-12-23 22:47:01 +0800

    add parser

---
+++ include/parser.hpp
@@ -0,0 +1,18 @@
+#include "tokenizer.hpp"
+
+struct Node {
+    std::string value;
+    std::vector<int> edges{0};
+};
+
+class RegGraph {
+    Node *head;
+    Node *tail;
+
+};
+
+
+class Parser {
+private:
+
+};
